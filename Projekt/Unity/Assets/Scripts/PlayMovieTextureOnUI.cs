fc9d201a2763ef2a39c63c44b9ed4ddc2b51a47b
Tim Unverzagt <tim.unverzagt@googlemail.com>
2017-05-21 01:03:11 +0200

    -added Unity Projekt / gitIgnore / webcam detection

---
+++ Projekt/Unity/Assets/Scripts/PlayMovieTextureOnUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMovieTextureOnUI : MonoBehaviour {
+
+    public RawImage rawImage;
+
+	// Use this for initialization
+    void Start()
+    {
+        
+        WebCamTexture webcamTexture = null;
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for(int i=0; i < devices.Length; i++) {
+            Debug.Log(devices[i].name);
+            if(devices[i].name.Equals("HD Webcam C525"))
+            {
+                webcamTexture = new WebCamTexture("HD Webcam C525");
+            } 
+        }
+        if (webcamTexture == null) {webcamTexture = new WebCamTexture(devices[0].name);}
+        
+        rawImage.texture = webcamTexture;
+        rawImage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+	
+}
